055d84bc95ab8acf80237d23fd756989d055d1b0
bash <bashxbuil@gmail.com>
2022-11-15 00:12:51 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # lovenorder
+# lovenorder
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const path = require('path')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static(path.join(__dirname + "/public")))
 
 // Database tables
 const db = require('./models')
